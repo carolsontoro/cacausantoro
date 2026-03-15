b080dc6bbcb386685ae46f10c074092e6c2ad76d
carolsontoro <anacarolliny.linx@gmail.com>
2026-03-15 17:54:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('I HATE U, CASCONA').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
